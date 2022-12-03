1256c6f31fa811dbd59a326e20ea65d51e54c4bd
Anjali Roland <anjaligroland@gmail.com>
2022-12-02 17:46:21 -0800

    js/css comments w/ outline of what needs to happen

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* When start button is cliked -------> event Bubble? (appendChild)?
+    1. timer starts ------------------> timerInterval + eventListener
+    2. first question is presented ---> eventListener
+*/
+
+
+/* When answer is selected -------------------------------------------------> event Bubble? (appendChild)?
+
+    - If answer is correct -------------------------------------------------> IF statement
+        1. 'Correct!' will appear under a faded line below the question  ---> dataAttribute? 
+        2.  User will be presented with another question -------------------> eventListener?
+
+    - Else (incorrect) -----------------------------------------------------> ELSE statement
+        1. 'Wrong ):' will appear under a faded line below the question ----> dataAttribute?
+        2. Time will be deducted from the clock ----------------------------> timeInterval
+        3. User will be presented with another question --------------------> eventListener?
+*/
+
+
+/* When all questions have been answered or timer reaches 0 ----> eventListener + timerInterval? 
+    
+    - User is presented with an end screen that includes: ------> eventListener
+        > their score ------------------------------------------> dataAttirbute + local storage?
+        > the option to save their initials and score ----------> local storage
+*/ 
